73cb90bef0c527a12d4150bd43187b530baffc2e
Moshe Berman <moshberm@gmail.com>
2014-07-04 18:42:10 -0400

    Simplified the LVC API a little bit.

--- LocationViewController/MapView/LVCMapView.h
+++ LocationViewController/MapView/LVCMapView.h
@@ -13,25 +13,6 @@
 @interface LVCMapView : UIImageView
 
 /**
- *  Converts a latitude and longitude to a CGPoint in the map view's coordinate space.
- *
- *  @param latitude The latitude to use.
- *  @param longiutde The longitude to use.
- *
- *  @return A CGPoint in the view's coordinate space.
- */
-
-- (CGPoint)pointFromLatitude:(CGFloat)latitude andLongitude:(CGFloat)longitude;
-
-/**
- *  Converts a local coordinate plane point to a lat/lon point.
- *
- *  @param point A CGPoint in the map's coordinate space.
- */
-
-- (CLLocationCoordinate2D)coordinateFromPoint:(CGPoint)point;
-
-/**
  *  Displays a marker on the given coordinate.
  */
 
