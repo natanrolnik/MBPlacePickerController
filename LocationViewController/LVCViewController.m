9c74843cec7d833b4d86d11fd782a7db7ebbddc6
Moshe Berman <moshefromsinai@gmail.com>
2014-07-04 03:46:33 -0400

    Added some code to handle a user location switch.

--- LocationViewController/LVCViewController.m
+++ LocationViewController/LVCViewController.m
@@ -22,6 +22,12 @@
 
 @property (weak, nonatomic) IBOutlet UISwitch *sortByContinentSwitch;
 
+/**
+ *  A switch to toggle display of user's location.
+ */
+
+@property (weak, nonatomic) IBOutlet UISwitch *showUserLocationSwitch;
+
 @end
 
 @implementation LVCViewController
@@ -34,7 +40,32 @@
 
 - (void)viewDidAppear:(BOOL)animated
 {
-        self.locationPickerController = [[LVCLocationPickerController alloc] init];
+
+    /**
+     *  Create the location picker now. 
+     *
+     *  If we create it too early, the map doesn't initialize correctly.
+     *  TODO: This is a bug, and should be fixed.
+     */
+    
+    self.locationPickerController = [[LVCLocationPickerController alloc] init];
+    
+    /**
+     *  Add a border to the buttons and views with the arbitrarily chosen tag 55.
+     */
+    
+    for (id view in self.view.subviews)
+    {
+        if ([view isKindOfClass:[UIButton class]] || [view tag] == 55)
+        {
+            UIButton *b = view;
+            
+            b.layer.borderColor = b.tintColor.CGColor;
+            b.layer.borderWidth = 1.0;
+            b.layer.cornerRadius = 5.0;
+        }
+    }
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,12 +80,21 @@
 }
 
 /**
- *
+ *  Toggles sorting by continent.
  */
 
 - (IBAction)toggleSortByContinent:(id)sender
 {
     [self.locationPickerController setSortByContinent:self.sortByContinentSwitch.on];
+}
+
+/**
+ *  Toggles the display of the user's location.
+ */
+
+- (IBAction)toggleShowUserLocation:(id)sender
+{
+    [self.locationPickerController setShowUserLocation:self.showUserLocationSwitch.on];
 }
 
 @end
