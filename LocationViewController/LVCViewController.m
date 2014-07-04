daacdaec3bef7fa0bff815e36a825620c229ef6d
Moshe Berman <moshefromsinai@gmail.com>
2014-07-04 03:16:31 -0400

    Added a switch to the location picker demo to toggle continent sorting..

--- LocationViewController/LVCViewController.m
+++ LocationViewController/LVCViewController.m
@@ -11,6 +11,17 @@
 
 @interface LVCViewController ()
 
+/**
+ *  The location picker.
+ */
+
+@property (nonatomic, strong) LVCLocationPickerController *locationPickerController;
+/**
+ *  A switch to toggle the continent sort.
+ */
+
+@property (weak, nonatomic) IBOutlet UISwitch *sortByContinentSwitch;
+
 @end
 
 @implementation LVCViewController
@@ -21,16 +32,29 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+        self.locationPickerController = [[LVCLocationPickerController alloc] init];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)showLocationPickerController:(id)sender {
-    LVCLocationPickerController *locationPicker = [[LVCLocationPickerController alloc] init];
-    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:locationPicker];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:self.locationPickerController];
     
-    [locationPicker display];
+    [self.locationPickerController display];
+}
+
+/**
+ *
+ */
+
+- (IBAction)toggleSortByContinent:(id)sender
+{
+    [self.locationPickerController setSortByContinent:self.sortByContinentSwitch.on];
 }
 
 @end
