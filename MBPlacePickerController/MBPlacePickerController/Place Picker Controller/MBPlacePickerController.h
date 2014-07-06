21bfc0157c42e38f6ae1e8b3bc47a4bf93d454d8
Moshe Berman <moshberm@gmail.com>
2014-07-06 00:59:08 -0400

    Updated the folder names for the picker and map view.

---
+++ MBPlacePickerController/MBPlacePickerController/Place Picker Controller/MBPlacePickerController.h
@@ -0,0 +1,81 @@
+//
+//  MBPlacePickerController.h
+//  MBPlacePickerController
+//
+//  Created by Moshe on 6/23/14.
+//  Copyright (c) 2014 Corlear Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MBMapView.h"
+#import "MBPlacePickerDelegate.h"
+
+@interface MBPlacePickerController : UIViewController
+
+/**
+ *  A flag to determine if the locations should be sorted by continent.
+ *  Default is YES.
+ */
+
+@property (nonatomic, assign) BOOL sortByContinent;
+
+/**
+ *  A URL to use to update the location list.
+ *  The default points to the GitHub repo.
+ */
+
+@property (nonatomic, strong) NSString *serverURL;
+
+/**
+ *  A view that renders the map.
+ */
+
+@property (nonatomic, strong, readonly) MBMapView *map;
+
+/**
+ *  A delegate to handle location changes.
+ */
+
+@property (nonatomic, assign) id<MBPlacePickerDelegate> delegate;
+
+/**
+ *  The working location.
+ */
+
+@property (nonatomic, strong) CLLocation *location;
+
+#pragma mark - Presenting and Dismissing the Picker
+
+/** ---
+ *  @name Presenting and Dismissing the Picker
+ *  ---
+ */
+
+/**
+ *  Asks the rootViewController of the keyWindow to display the location view controller.
+ */
+
+- (void)display;
+
+/**
+ *  Asks the rootViewController of the keyWindow to dismiss whatever it's showing.
+ */
+
+- (void)dismiss;
+
+/** ---
+ *  @name Update the location database
+ *  ---
+ */
+/**
+ *  Updates the location data from the server, then reloads the tableview.
+ */
+
+- (void)refreshLocationsFromServer;
+
+/**
+ *
+ *
+ */
+
+@end
